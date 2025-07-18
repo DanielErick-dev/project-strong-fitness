753e422c3b8a7241771450dab4c2e53f281722ea
Daniel Erick <DanielErick.estudante@gmail.com>
2025-07-17 21:38:17 -0300

    fix(login): fix login route

--- core/settings.py
+++ core/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     "widget_tweaks",
 ]
 
-LOGIN_URL = 'login'
+LOGIN_URL = '/'
 
 LOGIN_REDIRECT_URL = '/students/list/'
 
@@ -59,15 +59,15 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 DATABASES = {
-    "postgres": {
+    "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": config('POSTGRES_NAME'),
+        "NAME": config('POSTGRES_DB'),
         "USER": config('POSTGRES_USER'),
         "PASSWORD": config('POSTGRES_PASSWORD'),
         "HOST": config('POSTGRES_HOST'),
         "PORT": config('POSTGRES_PORT'),
     },
-    'default': {
+    'sql': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
