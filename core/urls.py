62340587ee4782b6f8d104f9509f5ed015fd31c7
Daniel Erick <DanielErick.estudante@gmail.com>
2025-07-30 23:28:01 -0300

    test(urls): create test urls to debug

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from django.contrib.auth import views
 from django.conf import settings
@@ -8,4 +9,5 @@ urlpatterns = [
     path("students/", include("students.urls")),
     path("", views.LoginView.as_view(), name='login'),
     path("logout/", views.LogoutView.as_view(), name='logout'),
+    path('teste/', lambda request: HttpResponse("<h1>A aplicação está respondendo!</h1>")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
